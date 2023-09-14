9468cf7847d82871662f8cc6b42741a5ba22b323
kcoffical6 <99400211+kcoffical6@users.noreply.github.com>
2023-09-14 19:01:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -77,13 +77,13 @@ async def start_command(client: Client, message: Message):
 
             try:
                 media = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                alert = await message.reply('Don,t Click Here</b> <a href="https://t.me/Kan_Serial"></a>')
+                alert = await message.reply('<a href="https://t.me/Kan_Serial">Don,t Click Here</a>')
                 await asyncio.sleep(30)
                 await alert.delete()
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                 media = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                alert = await message.reply('Don,t Click Here</b> <a href="https://t.me/Kan_Serial"></a>')
+                media = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                alert = await message.reply('<a href="https://t.me/Kan_Serial">Don,t Click Here</a>')
                 await asyncio.sleep(30)
                 await alert.delete()
             except:
